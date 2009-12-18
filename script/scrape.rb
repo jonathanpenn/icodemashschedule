2f6cfdffab8f1f4902a4d2d4a956915e960c2ead
Jonathan Penn <jonathan@wavethenavel.com>
2009-12-17 22:26:59 -0500

    Edited scrape file to deposit ics data in repo.

--- script/scrape.rb
+++ script/scrape.rb
@@ -2,14 +2,13 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-repo_dir = File.dirname(File.dirname(__FILE__))
+data_dir = File.join(File.dirname(File.dirname(__FILE__)), "data")
 
 calendar_feed = 'http://www.google.com/calendar/ical/h6s2ai0gdmaomi2pmlqkjiq4e0%40group.calendar.google.com/public/basic.ics'
 
 xml = open(calendar_feed).read
 
-FileUtils.mkdir_p(File.join(repo_dir, "tmp"))
-File.open(File.join(repo_dir, "tmp", "codemash.ics"), "w") do |f|
+File.open(File.join(data_dir, "codemash.ics"), "w") do |f|
   f.write xml
 end
 
