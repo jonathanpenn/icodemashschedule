e30e4788d73820a859d43bd536f68ecbbf2f7175
Jonathan Penn <jonathan@wavethenavel.com>
2009-12-17 23:51:14 -0500

    Sort the presentations by start time and output with rooms.

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -3,9 +3,13 @@ $(document).ready(function() {
 
   $("#canvas").append("<div id='schedule'></div>");
 
+  presentations.sort(function(a,b) {
+    return a.startTime > b.startTime ? 1 : -1;
+  });
+
   $.each(presentations, function(index, presentation) {
     $("#schedule").append("\
-      <li>"+presentation.title+"</li>\
+      <li>"+presentation.title+" ("+presentation.location+")</li>\
     ");
   });
 
