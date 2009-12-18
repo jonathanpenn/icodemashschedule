541eac0a030a7fed91960b59368d53bbdeab932d
Jonathan Penn <jonathan@wavethenavel.com>
2009-12-17 21:34:40 -0500

    Fixed scraping script

--- script/scrape.rb
+++ script/scrape.rb
@@ -2,14 +2,14 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-script_dir = File.dirname(__FILE__)
+repo_dir = File.dirname(File.dirname(__FILE__))
 
 calendar_feed = 'http://www.google.com/calendar/ical/h6s2ai0gdmaomi2pmlqkjiq4e0%40group.calendar.google.com/public/basic.ics'
 
 xml = open(calendar_feed).read
 
-File.mkdir_p(File.join(script_dir, "tmp"))
-File.open(File.join(script_dir, "tmp", "codemash.ics"), "w") do |f|
+FileUtils.mkdir_p(File.join(repo_dir, "tmp"))
+File.open(File.join(repo_dir, "tmp", "codemash.ics"), "w") do |f|
   f.write xml
 end
 
