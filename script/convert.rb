023ea6e18a3ececd2b745274e02376aa7a3b1159
Jonathan Penn <jonathan@wavethenavel.com>
2009-12-17 22:29:01 -0500

    Convert script saves schedule data as html.

--- script/convert.rb
+++ script/convert.rb
@@ -2,25 +2,29 @@ require 'rubygems'
 require 'vpim'
 require 'bluecloth'
 
-ical_data = File.read("tmp/codemash.ics")
+data_dir = File.join(File.dirname(File.dirname(__FILE__)), "data")
+ical_data = File.read(File.join(data_dir, "codemash.ics"))
 
 ical = Vpim::Icalendar.decode(ical_data)
-puts ical.first.events.first.inspect
 
 def to_html(text)
   BlueCloth.new(text).to_html
 end
 
-ical.first.events.each do |event|
+File.open(File.join(data_dir, "codemash.html"), "w") do |f|
 
-  result = <<-EOHTML
-    <div room='#{event.location}' startTime='#{event.dtstart}' endTime='#{event.dtend}'>
-      <h2>#{to_html(event.summary)}</h2>
-      <div class='description'>
-        #{to_html(event.description)}
+  ical.first.events.each do |event|
+
+    result = <<-EOHTML
+      <div room='#{event.location}' startTime='#{event.dtstart}' endTime='#{event.dtend}'>
+        <h2>#{to_html(event.summary)}</h2>
+        <div class='description'>
+          #{to_html(event.description)}
+        </div>
       </div>
-    </div>
-  EOHTML
+    EOHTML
 
-  puts result
+    f.puts result
+  end
+
 end
