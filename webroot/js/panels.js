6f065198ea52c4e7500d45928a6dd69527bf298a
Jonathan Penn <jonathan@wavethenavel.com>
2009-12-19 18:47:38 -0500

    Added panel backbutton features and ability to add content.

--- webroot/js/panels.js
+++ webroot/js/panels.js
@@ -1,28 +1,37 @@
 function Panel(initData)
 {
 
-  if (initData) {
-    this.id = initData.id;
-    this.title = initData.title;
-    this.content = initData.content;
-  } else {
-    this.id = 'panel_' + Panel.id_counter;
-    Panel.id_counter++;
-    this.title = "New Panel";
-    this.content = "Content";
-  }
+  if (!initData) { initData = {}; }
+
+  this.id = initData.id || Panel.generateId();
+  this.title = initData.title;
+  this.content = initData.content || '';
+  this.backPanelId = initData.backPanelId || '';
+  this.backButtonTitle = initData.backButtonTitle || 'Back';
 
 
   this.render = function()
   {
-    return "\
+    var $html = $("\
       <div id='"+this.id+"'>\
         <div class='toolbar'>\
+          <a class='back' href='#"+this.backPanelId+"'>\
+            "+this.backButtonTitle+"\
+          </a>\
           <h1>"+this.title+"</h1>\
         </div>\
-        "+this.content+"\
       </div>\
-    ";
+    ");
+
+    $html.append(this.content);
+
+    return $html;
+  }
+
+
+  this.appendContent = function(content)
+  {
+    this.content += content;
   }
 
 
@@ -30,3 +39,9 @@ function Panel(initData)
 
 
 Panel.id_counter = 1;
+
+
+Panel.generateId = function()
+{
+  return "panel_" + Panel.id_counter++;
+}
--- webroot/js/test/panels_test.js
+++ webroot/js/test/panels_test.js
@@ -11,9 +11,9 @@ $(document).ready(function() {
     expect(4);
 
     ok( panel.id == 'panel_1', "defaults panel id" );
-    ok( panel.title == 'New Panel', "defaults title" );
-    ok( panel.content == 'Content', "defaults content" );
     ok( (new Panel()).id == "panel_2", "increments the panel id counter" );
+    ok( panel.backPanelId == '', "back button just goes back to previous id" );
+    ok( panel.backButtonTitle == 'Back', "back button title" );
   });
 
 
@@ -21,13 +21,17 @@ $(document).ready(function() {
     var panel = new Panel({
       id: 'new_id',
       title: 'Panel 1',
-      content: 'Butterscotch'
+      content: 'Butterscotch',
+      backPanelId: 'panel_0',
+      backButtonTitle: 'Go Back'
     });
 
-    expect(3);
+    expect(5);
     ok( panel.id == 'new_id', "initializes the id" );
     ok( panel.title == 'Panel 1', "initializes the title" );
     ok( panel.content == 'Butterscotch', "initializes the content" );
+    ok( panel.backPanelId == 'panel_0', "initializes the back panel id" );
+    ok( panel.backButtonTitle == 'Go Back', "initializes back button title" );
   });
 
 
@@ -35,16 +39,38 @@ $(document).ready(function() {
     var panel = new Panel({
       id: 'butter_id',
       title: 'Panel 1',
-      content: '<div>Butterscotch</div>'
+      content: '<div>Butterscotch</div>',
+      backPanelId: 'panel_0',
+      backButtonTitle: 'Go Back'
     });
-    var $panel = $(panel.render());
+    var $panel = panel.render();
 
-    expect(3);
+    expect(5);
     ok( $panel.attr('id') == 'butter_id', "sets the dom id for the panel" );
     ok( $panel.find('> div.toolbar > h1').text() == 'Panel 1',
       "has h1 for title inside a toolbar div" );
     ok( $panel.find("> div.toolbar").next().html() == "Butterscotch",
       "appends content after toolbar" );
+    ok( $panel.find("> div.toolbar > a.back").attr('href') == "#panel_0",
+      "href for back button is the back panel id" );
+    ok( $panel.find("> div.toolbar > a.back").text().indexOf("Go Back") >= 0,
+      "title of backbutton is set" );
+  });
+
+
+  test(".appendContent(content)", function() {
+    var panel = new Panel({
+      id: 'butter_id',
+      title: 'Panel 1',
+      content: '<div>Butterscotch</div>'
+    });
+
+    panel.appendContent("<div>another</div>");
+    var $panel = panel.render();
+
+    expect(1);
+    ok( $panel.find('> div.toolbar').next().next().html() == "another",
+      "it adds content at the end of the panel div" );
   });
 
 
