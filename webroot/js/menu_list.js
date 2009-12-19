f3ca4bb37498db677e6763f29740517be00e3435
Jonathan Penn <jonathan@wavethenavel.com>
2009-12-19 18:57:42 -0500

    Fleshing out menu list objects. They can render themselves.

--- webroot/js/menu_list.js
+++ webroot/js/menu_list.js
@@ -1,6 +1,20 @@
 function MenuList(initData)
 {
   if (!initData) { initData = {}; }
+
+  this.items = initData.items || [];
+
+
+  this.$render = function()
+  {
+    var $ul = $("<ul class='rounded'></ul>");
+    $.each(this.items, function(index, item) {
+      $ul.append(item.$render());
+    });
+
+    return $ul;
+  }
+
 }
 
 
@@ -9,5 +23,15 @@ function MenuListItem(initData)
   if (!initData) { initData = {}; }
 
   this.title = initData.title;
-  this.panelId = initData.panelId;
+  this.panel = initData.panel;
+
+
+  this.$render = function()
+  {
+    return $("\
+      <li class='arrow'>\
+        <a href='#"+this.panel+"'>"+this.title+"</a>\
+      </li>\
+    ");
+  }
 }
--- webroot/js/test/menu_list_test.js
+++ webroot/js/test/menu_list_test.js
@@ -1,17 +1,47 @@
 $(document).ready(function() {
 
+  var menu;
 
-  module("MenuList");
+  function MockMenuListItem(renderResult)
+  {
+    this.$render = function()
+    {
+      return $("<li></li>").append(renderResult);
+    }
+  }
+
+
+  function setup()
+  {
+    menu = new MenuList({
+      items: [
+        new MockMenuListItem("first"),
+        new MockMenuListItem("second")
+      ]
+    });
+  }
+
+
+  module("MenuList", {setup:setup});
 
 
   test("initialiation", function() {
-    var menu = new MenuList({
-    });
-
     expect(1);
-    ok( menu, "menu object exists" );
+    ok( menu.items.length == 2, "menu has items" );
   });
 
+
+  test(".$render()", function() {
+    var $html = menu.$render();
+
+    expect(4);
+    ok( $html[0].tagName == 'UL', "is an unordered list" );
+    ok( $html.hasClass("rounded"), "is rounded" );
+    ok( $html.find("> li:nth(0)").html() == 'first',
+      "calls $render on the first menu item" );
+    ok( $html.find("> li:nth(1)").html() == 'second',
+      "calls $render on the second menu item" );
+  });
 
 
 });
@@ -19,19 +49,36 @@ $(document).ready(function() {
 
 $(document).ready(function() {
 
+  var item;
 
-  module("MenuListItem");
+
+  function setup()
+  {
+    item = new MenuListItem({
+      title: "title to click",
+      panel: "go_here"
+    });
+  }
+
+
+  module("MenuListItem", {setup:setup});
 
 
   test("initialization", function() {
-    var item = new MenuListItem({
-      title: "title to click",
-      panelId: "go_here"
-    });
-
     expect(2);
     ok( item.title == 'title to click', "has the title" );
-    ok( item.panelId == 'go_here', "has the panel id" );
+    ok( item.panel == 'go_here', "has the panel id" );
   });
+
+
+  test(".$render() as a link", function() {
+    var $html = item.$render();
+    expect(4);
+    ok( $html[0].tagName == 'LI', "is a list item" );
+    ok( $html.hasClass("arrow"), "with the class .arrow" );
+    ok( $html.find("> a").html() == "title to click", "link has given title" );
+    ok( $html.find("> a").attr('href') == "#go_here", "links to panel" );
+  });
+
 
 });
