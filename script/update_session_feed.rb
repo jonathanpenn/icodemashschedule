ef2a4c3c933919019b940abed7abdeb163e3425d
Jonathan Penn <jonathan@wavethenavel.com>
2009-12-23 00:26:31 -0500

    Renamed script to update_session_feed

--- script/update_session_feed.rb
+++ script/update_session_feed.rb
@@ -4,11 +4,11 @@ require 'open-uri'
 
 data_dir = File.join(File.dirname(File.dirname(__FILE__)), "data")
 
-calendar_feed = 'http://www.google.com/calendar/ical/h6s2ai0gdmaomi2pmlqkjiq4e0%40group.calendar.google.com/public/basic.ics'
+session_feed = 'http://www.codemash.org/rest/sessions.xml'
 
 xml = open(calendar_feed).read
 
-File.open(File.join(data_dir, "codemash.ics"), "w") do |f|
+File.open(File.join(data_dir, "codemash.xml"), "w") do |f|
   f.write xml
 end
 
