870be6a13459366bba5d6a31021c6abf094e8452
Jonathan Penn <jonathan@wavethenavel.com>
2010-01-01 10:48:21 -0500

    Map back button now fades in instead of sliding.

--- webroot/js/map.js
+++ webroot/js/map.js
@@ -4,9 +4,13 @@ $(document).ready(function() {
   var $back = $mapPanel.find("a.back");
   var leftDelta = 8, topDelta = 8;
   var positionTimer;
+  var lastX, lastY;
+  var buttonOpacity = '0.7';
 
   $mapPanel.bind("pageAnimationStart", function(event, info) {
     if (info.direction == 'in') {
+      lastX = lastY = null;
+      $back.css({ opacity: '0' });
       scrolling();
     } else {
       clearTimeout(positionTimer);
@@ -14,18 +18,25 @@ $(document).ready(function() {
     }
   });
 
-  $back.css({
-    opacity: '0.7'
-  });
-
-
   function scrolling()
   {
-    $back.stop().animate({
-      top: $(document).scrollTop() + topDelta + "px",
-      left: $(document).scrollLeft() + leftDelta + "px"
-    }, 300);
     positionTimer = setTimeout(scrolling, 500);
+
+    var nowY = $(document).scrollTop();
+    var nowX = $(document).scrollLeft();
+
+    if (nowY == lastY && nowX == lastX) { return; }
+
+    lastY = nowY;
+    lastX = nowX;
+
+    $back.stop().css({
+      top: nowY + topDelta + "px",
+      left: nowX + leftDelta + "px",
+      opacity: '0'
+    }).animate({
+      opacity: buttonOpacity
+    }, 800);
   }
 
 
