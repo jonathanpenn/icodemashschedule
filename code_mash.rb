2990421c6b538e58819007a7eb4a05f4adb296cb
Jonathan Penn <jonathan@wavethenavel.com>
2010-01-12 00:51:52 -0500

    Broke up the cache manifest into a rack app

--- code_mash.rb
+++ code_mash.rb
@@ -9,12 +9,6 @@ class CodeMash
 
   def call(env)
     case env['PATH_INFO']
-    when '/cache.manifest'
-      if HTML5_CACHING
-        render_cache_manifest
-      else
-        render_not_found
-      end
     when /\/(index|app)\.html/
       [200, {}, AppTemplate.render($1)]
     when '/test'
@@ -31,40 +25,6 @@ class CodeMash
       Redirecting to #{url}
     EOS
     [301, {'Location' => url}, text]
-  end
-
-  def render_not_found
-    [404, {}, '']
-  end
-
-  def render_cache_manifest
-    [200, {'Content-Type' => 'text/cache-manifest'}, manifest]
-  end
-
-
-  def manifest
-    <<EOM
-CACHE MANIFEST
-# For version #{APP_VERSION}
-#{file_list}
-EOM
-  end
-
-  def file_list
-    path = File.expand_path(File.dirname(__FILE__))
-    files = Dir[path+"/webroot/**/*"].reject do |file|
-      dont_include?(file)
-    end.map do |file|
-      file.gsub(path+"/webroot/",'')
-    end
-    files.join("\n")
-  end
-
-  def dont_include? file
-    File.directory?(file) ||
-      file =~ /\/test/ ||
-      file =~ /qunit/ ||
-      file =~ /nocache/
   end
 
 end
--- config.ru
+++ config.ru
@@ -1,5 +1,6 @@
 require 'rack'
 require 'code_mash'
+require 'cache_manifest'
 
 APP_VERSION = File.read(File.dirname(__FILE__) + '/VERSION')
 HTML5_CACHING = ENV['caching'] == 'false' ? false : true
@@ -7,6 +8,7 @@ HTML5_CACHING = ENV['caching'] == 'false' ? false : true
 use Rack::Deflater
 use Rack::ShowExceptions
 use Rack::ContentType, 'text/html'
+use CacheManifest
 use CodeMash
 use Rack::Static, :urls => ['/'], :root => 'webroot'
 run proc { [404, {}, ''] }
