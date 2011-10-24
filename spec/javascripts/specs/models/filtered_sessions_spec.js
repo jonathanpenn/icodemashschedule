5653763d74d56c98ada07f805ebfc30cf6777e00
Jonathan Penn <jonathan@navel-labs.com>
2011-10-23 20:02:45 -0400

    building a list of filtered sessions that can be dependent on another collection

--- spec/javascripts/specs/models/filtered_sessions_spec.js
+++ spec/javascripts/specs/models/filtered_sessions_spec.js
@@ -1,3 +1,58 @@
 describe("FilteredSessions", function() {
+  var sessions;
+  var filtered;
+
+  function resetSessions()
+  {
+    sessions.reset(
+      [
+        {
+          name: "bbb",
+          color: "white"
+        },
+        {
+          name: "ccc",
+          color: "blue"
+        },
+        {
+          name: "aaa",
+          color: "white"
+        }
+      ]
+    );
+  }
+
+  beforeEach(function() {
+    sessions = new Sessions();
+    filtered = new FilteredSessions({all_sessions: sessions});
+
+  });
+
+  describe("filtering and sorting", function() {
+    var resetCounter;
+
+    beforeEach(function() {
+      resetCounter = 0;
+      filtered.query = function(record) { return record.get('color') == 'white'; }
+      filtered.bind('reset', function() { resetCounter++; });
+      filtered.comparator = function(record) { return record.get('name'); }
+      resetSessions();
+    });
+
+    it("filters by the given filtering function", function() {
+      expect(filtered.models.length).toBe(2);
+    });
+
+    it("triggers a single reset event on the collection when filtered", function() {
+      expect(resetCounter).toBe(1);
+    });
+
+    it("sorts result by the comparator", function() {
+      var result = _.map(filtered.models, function(r) { return r.get('name'); });
+      expect(result).toEqual(['aaa', 'bbb']);
+    });
+
+  });
+
 });
 
