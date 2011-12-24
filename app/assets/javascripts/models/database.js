a8f34a2544bd77b6de30132df4bdc13b154f0c2f
Jonathan Penn <jonathan@navel-labs.com>
2011-12-24 00:23:18 -0500

    simplifying the global database

---
+++ app/assets/javascripts/models/database.js
@@ -0,0 +1,39 @@
+Database = {
+  sessions: null,
+
+  initialize: function(callback) {
+    this.loadFromLocalStorage();
+
+    var sessions = Database.sessions;
+
+    if (sessions.models.length == 0) {
+      $.mobile.showPageLoadingMsg();
+      Database.refreshFromServer(function() {
+        $.mobile.hidePageLoadingMsg();
+        if (callback) callback();
+      });
+    }
+  },
+
+  saveToLocalStorage: function() {
+    window.localStorage['sessions'] = this.sessions.serialize();
+  },
+
+  loadFromLocalStorage: function() {
+    var data = window.localStorage['sessions'];
+    if (!data) { this.sessions = new Sessions(); }
+    else { this.sessions = Sessions.deserialize(data); }
+  },
+
+  refreshFromServer: function(callback) {
+    if (!this.sessions) { this.sessions = new Sessions(); }
+
+    this.sessions.fetch({
+      complete: function() {
+        Database.saveToLocalStorage();
+        if (callback) callback();
+      }
+    });
+  }
+}
+
--- app/assets/javascripts/models/sessions.js
+++ app/assets/javascripts/models/sessions.js
@@ -42,30 +42,3 @@ Sessions.deserialize = function(data) {
   return new Sessions(JSON.parse(data));
 }
 
-SessionsDatastore = {
-  _localStorageKey: 'sessions',
-
-  sessions: null,
-
-  saveToLocalStorage: function() {
-    window.localStorage[this._localStorageKey] = this.sessions.serialize();
-  },
-
-  loadFromLocalStorage: function() {
-    var data = window.localStorage[this._localStorageKey];
-    if (!data) { this.sessions = new Sessions(); }
-    else { this.sessions = Sessions.deserialize(data); }
-  },
-
-  refreshFromServer: function(callback) {
-    if (!this.sessions) { this.sessions = new Sessions(); }
-
-    this.sessions.fetch({
-      complete: function() {
-        SessionsDatastore.saveToLocalStorage();
-        if (callback) callback();
-      }
-    });
-  }
-}
-
