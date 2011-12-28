25b4f6df50f0fc0a55f6549b476588e6bbeb4f5f
Jonathan Penn <jonathan@navel-labs.com>
2011-12-27 23:20:32 -0500

    group the sessions by hour

--- app/assets/javascripts/views/sessions_list_view.js
+++ app/assets/javascripts/views/sessions_list_view.js
@@ -8,18 +8,27 @@ var SessionsListView = Backbone.View.extend({
     var self = this;
     var $list = $(this.el);
 
-    this.collection.each(function(session) {
-      var item = new SessionListItemView({
-        model: session,
-        hideWhen: self.options.hideWhen,
-        hideRoom: self.options.hideRoom,
-        hideSpeaker: self.options.hideSpeaker
+    var groups = this.collection.groupByDate();
+    for (var date in groups) {
+      var sessions = groups[date];
+
+      var header = (new Date(date)).strftime("%I:%M %P").replace(/^0/,'').escapeHTML();
+
+      $list.append("<li data-role='list-divider'>"+header+"</li>");
+
+      _.each(sessions, function(session) {
+        var item = new SessionListItemView({
+          model: session,
+          hideWhen: self.options.hideWhen,
+          hideRoom: self.options.hideRoom,
+          hideSpeaker: self.options.hideSpeaker
+        });
+        $list.append(item.render().el);
+
       });
-      $list.append(item.render().el);
+    }
 
-      $list.listview('refresh');
-    });
-
+    $list.listview('refresh');
 
     return this;
   },
