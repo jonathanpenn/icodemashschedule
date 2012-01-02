6a47b1cdbeb9955d2cc813ecb3f743762076363b
Jonathan Penn <jonathan@navel-labs.com>
2012-01-01 23:58:50 -0500

    more tweaking to the auto cache manifest

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'cache_manifest'
 
 CodeMash::Application.routes.draw do
 
-  match "/application.manifest" => CacheManifest.new(Rails.env)
+  match "/application.manifest" => CacheManifest.new(Rails.env.production?)
   match "/javascript/specs" => "specs#index"
   match "/app.html" => "home#index"
   root to: redirect('/app.html')
--- lib/cache_manifest.rb
+++ lib/cache_manifest.rb
@@ -1,7 +1,7 @@
 class CacheManifest
 
-  def initialize environment
-    @environment = environment
+  def initialize production
+    @production = production
   end
 
   def call env
@@ -9,11 +9,9 @@ class CacheManifest
   end
 
   def body
-    @body ||= build
-  end
+    return @body if @body && Rails.env.production?
 
-  def build
-    <<EOS
+    @body = <<EOS
 CACHE MANIFEST
 # v#{cache_version}
 
@@ -26,21 +24,56 @@ EOS
   end
 
   def cache_files
-    @cache_files ||= asset_directories.map do |dir|
+    @cache_files ||= if production?
+      production_cache_files
+    else
+      development_cache_files
+    end
+  end
+
+  def production_cache_files
+    (development_cache_files.reject do |file|
+      file[/\.(js|css)$/]
+    end + ["/assets/application.js", "/assets/application.css"]).sort
+  end
+
+  def development_cache_files
+    asset_directories.map do |dir|
       Dir[dir+"/**/*"].map do |file|
-        if File.file?(file)
-          "/assets" + file.sub(dir, '').gsub(/\.erb$/,'')
+        if File.file?(file) && not_development?(file)
+          file = remove_directory_prefix(file, dir)
+          file = remove_any_development_extensions(file)
+          prepend_assets_path(file)
         end
       end
-    end.flatten.compact.sort
+    end.flatten.select{|v| v}.sort
+  end
+
+  def reject_directories(file)
+    File.file?(file)
+  end
+
+  def remove_directory_prefix(file, dir)
+    file.sub(dir, '')
+  end
+
+  def remove_any_development_extensions(file)
+    file.gsub(/\.erb$/, '')
+  end
+
+  def prepend_assets_path(file)
+    "/assets" + file
+  end
+
+  def not_development?(file)
+    !file[/\.(scss)/]
   end
 
   def asset_directories
     @asset_directories ||= Rails.application.config.assets.paths
   end
 
-  def production?; @environment == "production" end
-  def development?; @environment == "development" end
+  def production?; @production end
 
   def cache_version
     ENV['CACHE_VERSION'] || Time.now.to_f
