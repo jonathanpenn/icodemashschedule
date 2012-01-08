c3bcb2b9ebf36dfd1cf12801ae6a238684d5f407
Jonathan Penn <jonathan@navel-labs.com>
2012-01-08 15:59:37 -0500

    session slots now show more info if there's just one session

--- app/assets/javascripts/views/sessions_hour_list_item_view.js
+++ app/assets/javascripts/views/sessions_hour_list_item_view.js
@@ -10,18 +10,27 @@ var SessionsHourListItemView = Backbone.View.extend({
 
   render: function() {
     var slot = this.options.slot;
+    var sessions = slot.sessions();
+    var title, href;
+
+    if (sessions.models.length == 1) {
+      title = slot.hourName() + "<span class='innertitle'> - " + _.escape(sessions.models[0].title()) + "</span>";
+      href = "#" + sessions.models[0].pageId();
+    } else {
+      title = slot.hourName();
+      href = "#" + slot.pageId();
+    }
+
     var locals = {
-      title: slot.hourName(),
-      href: "#" + slot.pageId()
+      title: title,
+      href: href
     };
 
     var html = SessionsHourListItemView.template(locals);
 
     $(this.el).html(html);
 
-    var bubble = new FavoriteCountBubbleView({
-      sessions: slot.sessions()
-    });
+    var bubble = new FavoriteCountBubbleView({ sessions: sessions });
 
     $(this.el).find('a').append(bubble.render().el);
 
